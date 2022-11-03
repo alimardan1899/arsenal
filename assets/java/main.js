d956003d633ef724a25fa3c6028843a242f0cb3d
alimardan <alimardan.ismayilov@mail.ru>
2022-11-03 12:33:12 +0400

    dfjsjnvgjsn

--- assets/java/main.js
+++ assets/java/main.js
@@ -1,25 +1,58 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+let myNodelist = document.getElementsByTagName("LI");
+
+
+const filterButton = document.querySelector('.sekil');
+const ul = document.querySelector('#myUL');
+const lis = document.querySelector('#myUL').children;
+
+
+ul.addEventListener("click", (e) => {
+    if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+    }
+})
+
+filterButton.addEventListener("click", () => {
+    let arr = []
+
+    if (lis.length > 0) {
+        for (let el of lis) {
+            arr.push(el.firstChild.innerText)
+        }
+    }
+
+    arr.sort();
+
+    ul.innerHTML = ""
+    arr.forEach(item => {
+        ul.innerHTML += `  <li><p>${item}</p><span class="close">×</span></li>`
+    })
+
+
+})
+
+
+
+for (let i = 0; i < myNodelist.length; i++) {
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
 }
 
 
-var close = document.getElementsByClassName("close");
-var i;
+let close = document.getElementsByClassName("close");
+let i;
 for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-        var div = this.parentElement;
+        let div = this.parentElement;
         div.style.display = "none";
     }
 }
 
 
-var list = document.querySelector('ul');
+let list = document.querySelector('ul');
 list.addEventListener('click', function (ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
@@ -28,10 +61,14 @@ list.addEventListener('click', function (ev) {
 
 
 function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let s = document.createElement('p');
+    s.innerText = inputValue
+
+    //   let t = document.createTextNode(inputValue);
+    //     li.appendChild(t);
+    li.append(s)
     if (inputValue === '') {
         alert("Metni daxil edin");
     } else {
@@ -39,8 +76,8 @@ function newElement() {
     }
     document.getElementById("myInput").value = "";
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
@@ -51,4 +88,49 @@ function newElement() {
             div.style.display = "none";
         }
     }
-}tr,t
+}
+
+
+let sekil1 = document.querySelector('.sekil1')
+
+
+if (
+    sekil1.addEventListener("click", () => {
+        filterButton.style.display = "block";
+        sekil1.style.display = "none";
+        let arr = [];
+        list.push(arr);
+        arr.forEach((item) => {
+            item.sort();
+            item.reverse();
+        });
+    })
+) {
+}
+if (
+    filterButton.addEventListener("click", () => {
+        sekil1.style.display = "block";
+        filterButton.style.display = "none";
+    })
+) {
+}
+
+
+sekil1.addEventListener("click", () => {
+    let arr = []
+
+    if (lis.length > 0) {
+        for (let el of lis) {
+            arr.push(el.firstChild.innerText)
+        }
+    }
+
+    arr.sort();
+    arr.reverse()
+    ul.innerHTML = ""
+    arr.forEach(item => {
+        ul.innerHTML += `  <li><p>${item}</p><span class="close">×</span></li>`
+    })
+
+    
+})
